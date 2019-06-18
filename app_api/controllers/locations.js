5f46e95c1e2df6a5c548e1ca6a23cdacf0908a29
populationtwo <populationtwo@gmail.com>
2019-06-18 16:35:35 -0400

    Create location and reviews controller.

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Loc = mongoose.model("Location");
+
+const locationsListByDistance = (req, res) => {
+  res.status(200).json({ status: "success" });
+};
+const locationsCreate = (req, res) => {
+  res.status(200).json({ status: "success" });
+};
+const locationsReadOne = (req, res) => {
+  Loc.findById(req.params.locationid).exec((err, location) => {
+    if (!location) {
+      return res.status(404).json({ message: "location not found" });
+    } else if (err) {
+      return res.status(404).json(err);
+    }
+    res.status(200).json(location);
+  });
+};
+const locationsUpdateOne = (req, res) => {
+  res.status(200).json({ status: "success" });
+};
+const locationsDeleteOne = (req, res) => {
+  res.status(200).json({ status: "success" });
+};
+
+module.exports = {
+  locationsListByDistance,
+  locationsCreate,
+  locationsReadOne,
+  locationsUpdateOne,
+  locationsDeleteOne
+};
---
+++ app_api/controllers/reviews.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const Loc = mongoose.model("Location");
+
+const reviewsCreate = (req, res) => {
+  res.status(200).json({ status: "success" });
+};
+const reviewsReadOne = (req, res) => {
+  Loc.findById(req.params.locationid)
+    .select("name reviews")
+    .exec((err, location) => {
+      if (!location) {
+        return res.status(404).json({ message: "Location not found" });
+      } else if (err) {
+        return res.status(404).json(err);
+      }
+      if (location.reviews && location.reviews.length > 0) {
+        const review = location.reviews.id(req.params.reviewid);
+        if (!review) {
+          return res.status(400).json({
+            message: "Review not found"
+          });
+        } else {
+          let response = {
+            location: {
+              name: location.name,
+              id: req.params.locationid
+            },
+            review
+          };
+          return res.status(200).json(response);
+        }
+      } else {
+        return res.status(404).json({ message: "No reviews found" });
+      }
+    });
+};
+const reviewsUpdateOne = (req, res) => {
+  res.status(200).json({ status: "success" });
+};
+const reviewsDeleteOne = (req, res) => {
+  res.status(200).json({ status: "success" });
+};
+
+module.exports = {
+  reviewsCreate,
+  reviewsReadOne,
+  reviewsUpdateOne,
+  reviewsDeleteOne
+};
