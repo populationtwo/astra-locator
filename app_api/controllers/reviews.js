e2f2af550f579e2b09d00a8f8fa1c86726799e52
populationtwo <populationtwo@gmail.com>
2019-06-20 15:14:40 -0400

    Update review controller.

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,8 +1,68 @@
 const mongoose = require("mongoose");
 const Loc = mongoose.model("Location");
 
+const doSetAverageRating = location => {
+  if (location.reviews && location.reviews.length > 0) {
+    const count = location.reviews.length;
+    const total = location.reviews.reduce((acc, { rating }) => {
+      return acc + rating;
+    }, 0);
+    location.rating = parseInt(total / count, 10);
+    location.save(err => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`Average rating updated to ${location.rating}`);
+      }
+    });
+  }
+};
+const updateAverageRating = locationId => {
+  Loc.findById(locationId)
+    .select("rating reviews")
+    .exec((err, location) => {
+      if (!err) {
+        doSetAverageRating(location);
+      }
+    });
+};
+const doAddReview = (req, res, location) => {
+  if (!location) {
+    res.status(404).json({ status: "Location not found" });
+  } else {
+    const { author, rating, reviewText } = req.body;
+    location.reviews.push({
+      author,
+      rating,
+      reviewText
+    });
+    location.save((err, location) => {
+      if (err) {
+        res.status(400).json(err);
+      } else {
+        updateAverageRating(location._id);
+        const thisReview = location.reviews.slice(-1).pop();
+        res.status(201).json(thisReview);
+      }
+    });
+  }
+};
+
 const reviewsCreate = (req, res) => {
-  res.status(200).json({ status: "success" });
+  const locationId = req.params.locationid;
+  if (locationId) {
+    Loc.findById(locationId)
+      .select("reviews")
+      .exec((err, location) => {
+        if (err) {
+          res.status(404).json(err);
+        } else {
+          doAddReview(req, res, location);
+        }
+      });
+  } else {
+    res.status(404).json({ status: "Location not found" });
+  }
 };
 const reviewsReadOne = (req, res) => {
   Loc.findById(req.params.locationid)
