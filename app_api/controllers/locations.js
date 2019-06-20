6a218d1b4a0a393d1fea371f8d3d39c659b82505
populationtwo <populationtwo@gmail.com>
2019-06-20 17:06:22 -0400

    Create reviewsUpdateOne controller.

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -52,25 +52,25 @@ const locationsCreate = (req, res) => {
     {
       name: req.body.name,
       address: req.body.address,
-            facilities: req.body.facilities.split(","),
-            coords: {
-              type: "Point",
-              coordinates: [parseFloat(req.body.lng), parseFloat(req.body.lat)]
-            },
-            openingTimes: [
-              {
-                days: req.body.days1,
-                opening: req.body.opening1,
-                closing: req.body.closing1,
-                closed: req.body.closed1
-              },
-              {
-                days: req.body.days2,
-                opening: req.body.opening2,
-                closing: req.body.closing2,
-                closed: req.body.closed2
-              }
-            ]
+      facilities: req.body.facilities.split(","),
+      coords: {
+        type: "Point",
+        coordinates: [parseFloat(req.body.lng), parseFloat(req.body.lat)]
+      },
+      openingTimes: [
+        {
+          days: req.body.days1,
+          opening: req.body.opening1,
+          closing: req.body.closing1,
+          closed: req.body.closed1
+        },
+        {
+          days: req.body.days2,
+          opening: req.body.opening2,
+          closing: req.body.closing2,
+          closed: req.body.closed2
+        }
+      ]
     },
     (err, location) => {
       if (err) {
@@ -92,6 +92,50 @@ const locationsReadOne = (req, res) => {
   });
 };
 const locationsUpdateOne = (req, res) => {
+  if (!req.params.locationid) {
+    return res
+      .status(404)
+      .json({ message: "Not found, locationid is required." });
+  }
+
+  Loc.findbyId(req.params.locationid)
+    .select("-reviews -rating")
+    .exec((err, location) => {
+      if (!location) {
+        return res.json(404).status({ message: "locationid not found" });
+      } else if (err) {
+        return res.status(400).json(err);
+      }
+      location.name = req.body.name;
+      location.address = req.body.address;
+      location.facilities = req.body.facilities.split(",");
+      location.coords = {
+        type: "Point",
+        coordinates: [parseFloat(req.body.lng), parseFloat(req.body.lat)]
+      };
+      location.openingTimes = [
+        {
+          days: req.body.days1,
+          opening: req.body.opening1,
+          closing: req.body.closing1,
+          closed: req.body.closed1
+        },
+        {
+          days: req.body.days2,
+          opening: req.body.opening2,
+          closing: req.body.closing2,
+          closed: req.body.closed2
+        }
+      ];
+      location.save((err, loc) => {
+        if (err) {
+          res.status(404).json(err);
+        } else {
+          res.status(200).json(loc);
+        }
+      });
+    });
+
   res.status(200).json({ status: "success" });
 };
 const locationsDeleteOne = (req, res) => {
