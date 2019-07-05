a7b8dc665d8679b5d9d4d27e80002527ab113f55
populationtwo <populationtwo@gmail.com>
2019-07-05 15:46:01 -0400

    Create homepage component.

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -3,11 +3,34 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { HomeListComponent } from "./home-list/home-list.component";
 import { DistancePipe } from "./distance.pipe";
+import { RouterModule } from "@angular/router";
+import { FrameworkComponent } from "./framework/framework.component";
+import { AboutComponent } from "./about/about.component";
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
-  declarations: [HomeListComponent, DistancePipe],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    HomeListComponent,
+    DistancePipe,
+    FrameworkComponent,
+    AboutComponent,
+    HomepageComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: "",
+        component: HomeListComponent
+      },
+      {
+        path: "about",
+        component: AboutComponent
+      }
+    ])
+  ],
   providers: [],
-  bootstrap: [HomeListComponent]
+  bootstrap: [FrameworkComponent]
 })
 export class AppModule {}
