19a4a8bcff2c1ca37613de2055cbfc1fe4fea474
populationtwo <populationtwo@gmail.com>
2019-07-16 17:34:59 -0400

    Change reviews display order

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { HtmlLineBreaksPipe } from './html-line-breaks.pipe';
 import { RatingStarsComponent } from './rating-stars/rating-stars.component';
 import { LocationDetailsComponent } from './location-details/location-details.component';
 import { DetailsPageComponent } from './details-page/details-page.component';
+import { MostRecentFirstPipe } from './most-recent-first.pipe';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { DetailsPageComponent } from './details-page/details-page.component';
     HtmlLineBreaksPipe,
     RatingStarsComponent,
     LocationDetailsComponent,
-    DetailsPageComponent
+    DetailsPageComponent,
+    MostRecentFirstPipe
   ],
   imports: [
     BrowserModule,
