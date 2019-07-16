638a6eaf5df5312f29fdcb6ade4d43adf0bc555e
populationtwo <populationtwo@gmail.com>
2019-07-16 17:58:51 -0400

    Toggle form visibility

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RatingStarsComponent } from './rating-stars/rating-stars.component';
 import { LocationDetailsComponent } from './location-details/location-details.component';
 import { DetailsPageComponent } from './details-page/details-page.component';
 import { MostRecentFirstPipe } from './most-recent-first.pipe';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -46,7 +47,9 @@ import { MostRecentFirstPipe } from './most-recent-first.pipe';
         path: "location/:locationId",
         component: DetailsPageComponent
       }
-    ])
+    ]),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [FrameworkComponent]
--- app_public/src/app/location-details/location-details.component.ts
+++ app_public/src/app/location-details/location-details.component.ts
@@ -8,9 +8,14 @@ import { Location } from "../home-list/home-list.component";
 })
 export class LocationDetailsComponent implements OnInit {
   @Input() location: Location;
-
+  public formVisible: boolean = false;
   public googleAPIKey: string = "AIzaSyD936k9N-wBb16mUfR7Mhy_C0cWzSbUx3w";
 
+  public newReview = {
+    author: "",
+    rating: 5,
+    reviewText: ""
+  };
   constructor() {}
 
   ngOnInit() {}
