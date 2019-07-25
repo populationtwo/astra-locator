ce19458c9597f7cd7c192ae72e2677cf631eafbe
populationtwo <populationtwo@gmail.com>
2019-07-25 17:17:45 -0400

    Create history service.

--- app_public/src/app/framework/framework.component.ts
+++ app_public/src/app/framework/framework.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthenticationService } from "../authentication.service";
+import { User } from "../user";
+import { HistoryService } from "../history.service";
 
 @Component({
-  selector: 'app-framework',
-  templateUrl: './framework.component.html',
-  styleUrls: ['./framework.component.css']
+  selector: "app-framework",
+  templateUrl: "./framework.component.html",
+  styleUrls: ["./framework.component.css"]
 })
 export class FrameworkComponent implements OnInit {
+  constructor(private authenticationService: AuthenticationService, private historyService: HistoryService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public doLogout(): void {
+    this.authenticationService.logout();
   }
 
+  public isLoggedIn(): boolean {
+    return this.authenticationService.isLoggedIn();
+  }
+
+  getUsername(): string {
+    const user: User = this.authenticationService.getCurrentUser();
+    return user ? user.name : "Guest";
+  }
 }
